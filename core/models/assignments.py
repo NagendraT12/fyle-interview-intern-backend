3be189425ab2c4452c636432dd6e965528db159f
NagendraT12 <nagendrat121212@gmail.com>
2024-09-23 15:34:39 +0530

    Teachers test case completed task completed

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -89,6 +89,8 @@ class Assignment(db.Model):
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
 
+    # The second commentof teacher
     @classmethod
-    def get_assignments_by_teacher(cls):
-        return cls.query.all()
+    def get_assignments_by_teacher(cls,teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
+
