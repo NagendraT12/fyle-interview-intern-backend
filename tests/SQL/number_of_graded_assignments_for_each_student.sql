bc8c0253db4ad6539b52a638eccbc5b4d8e58faa
NagendraT12 <nagendrat121212@gmail.com>
2024-09-23 15:18:28 +0530

    Written a query

--- tests/SQL/number_of_graded_assignments_for_each_student.sql
+++ tests/SQL/number_of_graded_assignments_for_each_student.sql
@@ -1,1 +1,5 @@
 -- Write query to get number of graded assignments for each student:
+SELECT student_id, COUNT(*) AS number_of_graded_assign
+FROM assignments
+WHERE grade IS NOT NULL
+GROUP BY student_id;
